6e1cda1021e05d9c802fb918d4dd89ebedc33e5c
Jothiseeni <jothisiva6789@gmail.com>
2026-01-09 21:12:23 +0530

    created new file code.java

---
+++ Two-sum/longest-substring-without-repeating-character/Solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Solution {
+
+    public static int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int maxLength = 0;
+        Set<Character> charSet = new HashSet<>();
+        int left = 0;
+
+        for (int right = 0; right < n; right++) {
+            if (!charSet.contains(s.charAt(right))) {
+                charSet.add(s.charAt(right));
+                maxLength = Math.max(maxLength, right - left + 1);
+            } else {
+                while (charSet.contains(s.charAt(right))) {
+                    charSet.remove(s.charAt(left));
+                    left++;
+                }
+                charSet.add(s.charAt(right));
+            }
+        }
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        System.out.println("Longest substring length: " + lengthOfLongestSubstring(s));
+    }
+}
