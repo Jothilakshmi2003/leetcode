3b6d8cdb1cb73b91dd50ea2c97673661fa13d811
Jothiseeni <jothisiva6789@gmail.com>
2026-03-12 14:11:08 +0530

    created new file Solution.java

---
+++ Q&A/Maximize Spanning Tree Stability with Upgrades/Solution.java
@@ -0,0 +1,153 @@
+import java.util.*;
+
+class DSU {
+    int[] parent;
+    int[] rank;
+    int components;
+
+    public DSU(int n) {
+        parent = new int[n];
+        rank = new int[n];
+        components = n;
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    public int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    public boolean unite(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+
+        if (pa == pb)
+            return false;
+
+        if (rank[pa] < rank[pb]) {
+            int temp = pa;
+            pa = pb;
+            pb = temp;
+        }
+
+        parent[pb] = pa;
+
+        if (rank[pa] == rank[pb]) {
+            rank[pa]++;
+        }
+
+        components--;
+        return true;
+    }
+}
+
+public class Solution {
+
+    public boolean canAchieve(int n, int[][] edges, int k, int x) {
+        DSU dsu = new DSU(n);
+
+        // Mandatory edges
+        for (int[] e : edges) {
+            int u = e[0], v = e[1], s = e[2], must = e[3];
+
+            if (must == 1) {
+                if (s < x)
+                    return false;
+                if (!dsu.unite(u, v))
+                    return false;
+            }
+        }
+
+        // Free optional edges
+        for (int[] e : edges) {
+            int u = e[0], v = e[1], s = e[2], must = e[3];
+
+            if (must == 0 && s >= x) {
+                dsu.unite(u, v);
+            }
+        }
+
+        // Upgrade edges
+        int usedUpgrades = 0;
+
+        for (int[] e : edges) {
+            int u = e[0], v = e[1], s = e[2], must = e[3];
+
+            if (must == 0 && s < x && 2 * s >= x) {
+                if (dsu.unite(u, v)) {
+                    usedUpgrades++;
+                    if (usedUpgrades > k)
+                        return false;
+                }
+            }
+        }
+
+        return dsu.components == 1;
+    }
+
+    public int maxStability(int n, int[][] edges, int k) {
+
+        // Check mandatory cycle
+        DSU dsu = new DSU(n);
+
+        for (int[] e : edges) {
+            if (e[3] == 1) {
+                if (!dsu.unite(e[0], e[1])) {
+                    return -1;
+                }
+            }
+        }
+
+        int low = 1, high = 200000;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (canAchieve(n, edges, k, mid)) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    // Main method for VS Code
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Solution sol = new Solution();
+
+        System.out.print("Enter number of nodes: ");
+        int n = sc.nextInt();
+
+        System.out.print("Enter number of edges: ");
+        int m = sc.nextInt();
+
+        int[][] edges = new int[m][4];
+
+        System.out.println("Enter edges (u v stability must[0/1]):");
+        for (int i = 0; i < m; i++) {
+            edges[i][0] = sc.nextInt();
+            edges[i][1] = sc.nextInt();
+            edges[i][2] = sc.nextInt();
+            edges[i][3] = sc.nextInt();
+        }
+
+        System.out.print("Enter number of upgrades k: ");
+        int k = sc.nextInt();
+
+        int result = sol.maxStability(n, edges, k);
+
+        System.out.println("Maximum Stability: " + result);
+
+        sc.close();
+    }
+}
